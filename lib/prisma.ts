14b4f3e2fc3e3d112b73cdef89ae3a7d1ee4fc90
SIDNEYHOLMES <sidneyselassie1@gmail.com>
2024-02-23 23:46:49 -0600

    Prism database connected to google provider next auth

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+declare global {
+  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
+}
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+import { PrismaClient } from "@prisma/client";
+let prisma: PrismaClient;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-export default prisma
+export default prisma;
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,39 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-
-const prisma = new PrismaClient()
+import prisma from '../../../../../lib/prisma'
 
 const handler = NextAuth({
-  // adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt'
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if(!profile?.email) {
+        throw new Error('No Profile')
+      }
+
+      await prisma.user.upsert({
+        where: {
+          email: profile.email,
+        },
+        create: {
+          email: profile.email,
+          name: <string> profile.name,
+        },
+        update: {
+          name: profile.name,
+        }
+      })
+
+      return true;
+    }
+  }
 })
 
 export { handler as GET, handler as POST}
